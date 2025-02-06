6017309ddb131b64aab88196d7967311544c0631
samikannup53 <samikannup53@gmail.com>
2025-02-06 21:45:13 +0530

    Added Transfer Data From Form to Table Function in JS File

--- script.js
+++ script.js
@@ -5,17 +5,57 @@ let addButton = document.getElementById("addButton");
 let resetButton = document.getElementById("resetButton");
 let transactionData = document.getElementById("transactionData");
 
-let transactionDataBase = [];
+const transactionDataBase = [
+  // { catagory: "income", discription: "salary", amount: 500 },
+  // { catagory: "expense", discription: "ticket", amount: 400 },
+  // { catagory: "income", discription: "salary", amount: 300 },
+  // { catagory: "expense", discription: "petrol", amount: 200 },
+  // { catagory: "income", discription: "salary", amount: 100 },
+];
+
+let transactions = transactionDataBase;
+
+function loadTranscationDetails(transaction) {
+  let table = document.getElementById("transactionData");
+  let newRow = table.insertRow(table.length);
+
+  cell1 = newRow.insertCell();
+  cell1.innerHTML = transactions.length;
+
+  cell2 = newRow.insertCell();
+  cell2.innerHTML =
+    transaction.catagory === "income" ? transaction.amount : "--";
+
+  cell3 = newRow.insertCell();
+  cell3.innerHTML =
+    transaction.catagory === "expense" ? transaction.amount : "--";
+
+  cell4 = newRow.insertCell();
+  cell4.innerHTML = transaction.discription;
+
+  cell5 = newRow.insertCell();
+  cell5.innerHTML = transaction.date;
+
+  cell6 = newRow.insertCell();
+  cell6.innerHTML = `
+    <div class="action-cta">
+        <button id="deleteBtn"><i class="bi bi-trash3-fill"></i></Button>
+        <button id="editBtn"><i class="bi bi-pencil-square"></i></button>
+    </div>`;
+}
 
 function addTransaction() {
-  const catagory = catagoryInput.value;
-  const amount = Number(amountInput.value);
-  const discription = discriptionInput.value;
-
   formValidation();
 
-  transactionDataBase.push({ catagory, amount, discription });
-  console.log(transactionDataBase);
+  const transaction = {
+    catagory: catagoryInput.value,
+    discription: discriptionInput.value,
+    amount: amountInput.value,
+  };
+
+  transactionDataBase.push(transaction);
+
+  loadTranscationDetails(transaction);
 }
 
 function formValidation() {
@@ -45,3 +85,12 @@ function resetEntry() {
 }
 
 resetButton.addEventListener("click", resetEntry);
+
+function config() {
+  transactionData.innerHTML = "";
+  transactions.forEach(loadTranscationDetails);
+}
+
+window.addEventListener("load", function () {
+  config();
+});
