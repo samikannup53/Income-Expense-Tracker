ece2241b6022abcb722aad5ce786c133c15aced8
samikannup53 <samikannup53@gmail.com>
2025-02-08 20:35:29 +0530

    Major Update in JS File

--- script.js
+++ script.js
@@ -1,66 +1,95 @@
 let catagoryInput = document.getElementById("catagoryInput");
 let amountInput = document.getElementById("amountInput");
 let discriptionInput = document.getElementById("discriptionInput");
+let dateInput = document.getElementById("dateInput");
 let addButton = document.getElementById("addButton");
 let resetButton = document.getElementById("resetButton");
 let transactionData = document.getElementById("transactionData");
+let applyFilterBtn = document.getElementById("applyFilterBtn");
+let totalIncomeDisplay = document.getElementById("totalIncomeDisplay");
+let formDashboardBalance = document.getElementById("formDashboardBalance");
+let headerBalance = document.getElementById("headerBalance");
 
-const transactionDataBase = [
-  // { catagory: "income", discription: "salary", amount: 500 },
-  // { catagory: "expense", discription: "ticket", amount: 400 },
-  // { catagory: "income", discription: "salary", amount: 300 },
-  // { catagory: "expense", discription: "petrol", amount: 200 },
-  // { catagory: "income", discription: "salary", amount: 100 },
-];
+let dataBase = [];
+let totalIncome = 0;
+let totalExpense = 0;
+let netBalance = 0;
 
-let transactions = transactionDataBase;
+// Adding Data to Local Storage
+function addData() {
+  localStorage.setItem("dataBase", JSON.stringify(dataBase));
+}
 
-function loadTranscationDetails(transaction) {
-  let table = document.getElementById("transactionData");
-  let newRow = table.insertRow(table.length);
+//Getting Data From Local Storage
+function getData() {
+  const storedData = localStorage.getItem("dataBase");
+  if (storedData) {
+    dataBase = JSON.parse(storedData);
+    dataTable(dataBase);
+    updateTotal();
+  }
+}
 
-  cell1 = newRow.insertCell();
-  cell1.innerHTML = transactions.length;
+// Getting Total Income, Expense & Net Balance Values
+function updateTotal() {
+  totalIncome = dataBase
+    .filter((data) => data.catagory === "income")
+    .reduce((sum, data) => sum + data.amount, 0);
+  totalIncomeDisplay.textContent = `₹ ${totalIncome.toFixed(2)}`;
 
-  cell2 = newRow.insertCell();
-  cell2.innerHTML =
-    transaction.catagory === "income" ? transaction.amount : "--";
+  totalExpense = dataBase
+    .filter((data) => data.catagory === "expense")
+    .reduce((sum, data) => sum + data.amount, 0);
+  totalExpenseDisplay.textContent = `₹ ${totalExpense.toFixed(2)}`;
 
-  cell3 = newRow.insertCell();
-  cell3.innerHTML =
-    transaction.catagory === "expense" ? transaction.amount : "--";
+  netBalance = totalIncome - totalExpense;
+  headerBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
+  formDashboardBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
+}
 
-  cell4 = newRow.insertCell();
-  cell4.innerHTML = transaction.discription;
+function dataTable(filteredData) {
+  transactionData.innerHTML = "";
 
-  cell5 = newRow.insertCell();
-  cell5.innerHTML = transaction.date;
+  filteredData.forEach((data, index) => {
+    let table = document.getElementById("transactionData");
+    let newRow = table.insertRow(table.length);
 
-  cell6 = newRow.insertCell();
-  cell6.innerHTML = `
+    cell1 = newRow.insertCell();
+    cell1.innerHTML = index + 1;
+
+    cell2 = newRow.insertCell();
+    cell2.innerHTML = data.catagory === "income" ? data.amount : "--";
+
+    cell3 = newRow.insertCell();
+    cell3.innerHTML = data.catagory === "expense" ? data.amount : "--";
+
+    cell4 = newRow.insertCell();
+    cell4.innerHTML = data.discription;
+
+    cell5 = newRow.insertCell();
+    cell5.innerHTML = data.entryDate;
+
+    cell6 = newRow.insertCell();
+    cell6.innerHTML = `
     <div class="action-cta">
         <button id="deleteBtn"><i class="bi bi-trash3-fill"></i></Button>
         <button id="editBtn"><i class="bi bi-pencil-square"></i></button>
     </div>`;
+  });
 }
 
 function addTransaction() {
-  formValidation();
+  const catagory = catagoryInput.value;
+  const discription = discriptionInput.value;
+  const amount = Number(amountInput.value);
+  const entryDate = dateInput.value;
 
-  const transaction = {
-    catagory: catagoryInput.value,
-    discription: discriptionInput.value,
-    amount: amountInput.value,
-  };
-
-  transactionDataBase.push(transaction);
-
-  loadTranscationDetails(transaction);
-}
-
-function formValidation() {
   if (catagoryInput.value === "") {
     alert("Kindly Select the Catagory from Dropdown Menu !");
+    return;
+  }
+  if (dateInput.value === "") {
+    alert("Kindly Select Valid Transaction Date !");
     return;
   }
   if (
@@ -74,6 +103,12 @@ function formValidation() {
     alert("Kindly Fill Valid Discription !");
     return;
   }
+
+  dataBase.push({ catagory, discription, amount, entryDate });
+  dataTable(dataBase);
+  addData();
+  updateTotal();
+  resetEntry();
 }
 
 addButton.addEventListener("click", addTransaction);
@@ -82,15 +117,20 @@ function resetEntry() {
   catagoryInput.value = "";
   amountInput.value = "";
   discriptionInput.value = "";
+  dateInput.value = "";
 }
 
 resetButton.addEventListener("click", resetEntry);
 
-function config() {
-  transactionData.innerHTML = "";
-  transactions.forEach(loadTranscationDetails);
-}
+applyFilterBtn.addEventListener("click", () => {
+  const selectedData = document.querySelector(
+    'input[name="filter"]:checked'
+  ).value;
+  const filteredData = dataBase.filter(
+    (data) => selectedData === "all" || data.catagory === selectedData
+  );
 
-window.addEventListener("load", function () {
-  config();
+  dataTable(filteredData);
 });
+
+document.addEventListener("DOMContentLoaded", getData);
