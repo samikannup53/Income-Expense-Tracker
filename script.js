47650b0a92595a8fc1c845aaa6d9c1298c016827
samikannup53 <samikannup53@gmail.com>
2025-02-11 20:44:24 +0530

    Added Function to Delete and Edit Button in JS File

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declaring Global Variables
 let catagoryInput = document.getElementById("catagoryInput");
 let amountInput = document.getElementById("amountInput");
 let discriptionInput = document.getElementById("discriptionInput");
@@ -33,54 +34,10 @@ function getData() {
   }
 }
 
-// Getting Total Income, Expense & Net Balance Values
-function updateTotal() {
-  totalIncome = dataBase
-    .filter((data) => data.catagory === "income")
-    .reduce((sum, data) => sum + data.amount, 0);
-  totalIncomeDisplay.textContent = `₹ ${totalIncome.toFixed(2)}`;
+// Function to Add the Transaction after Clicking the Add Button
+addButton.addEventListener("click", addTransaction);
 
-  totalExpense = dataBase
-    .filter((data) => data.catagory === "expense")
-    .reduce((sum, data) => sum + data.amount, 0);
-  totalExpenseDisplay.textContent = `₹ ${totalExpense.toFixed(2)}`;
-
-  netBalance = totalIncome - totalExpense;
-  headerBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
-  formDashboardBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
-}
-
-function dataTable(filteredData) {
-  transactionData.innerHTML = "";
-
-  filteredData.forEach((data, index) => {
-    let table = document.getElementById("transactionData");
-    let newRow = table.insertRow(table.length);
-
-    cell1 = newRow.insertCell();
-    cell1.innerHTML = index + 1;
-
-    cell2 = newRow.insertCell();
-    cell2.innerHTML = data.catagory === "income" ? data.amount : "--";
-
-    cell3 = newRow.insertCell();
-    cell3.innerHTML = data.catagory === "expense" ? data.amount : "--";
-
-    cell4 = newRow.insertCell();
-    cell4.innerHTML = data.discription;
-
-    cell5 = newRow.insertCell();
-    cell5.innerHTML = data.entryDate;
-
-    cell6 = newRow.insertCell();
-    cell6.innerHTML = `
-    <div class="action-cta">
-        <button id="deleteBtn" onclick = deleteData()><i class="bi bi-trash3-fill"></i></Button>
-        <button id="editBtn" onclick = editData()><i class="bi bi-pencil-square"></i></button>
-    </div>`;
-  });
-}
-
+// Add Transaction Function
 function addTransaction() {
   const catagory = catagoryInput.value;
   const discription = discriptionInput.value;
@@ -107,21 +64,135 @@ function addTransaction() {
     return;
   }
 
-  dataBase.push({ catagory, discription, amount, entryDate });
-  dataTable(dataBase);
-  addData();
-  updateTotal();
-  resetEntry();
+  if (isEditing) {
+    dataBase[editIndex] = { catagory, discription, amount, entryDate };
+    isEditing = false;
+    editIndex = -1;
+    addButton.textContent = "Add";
+    addData();
+    updateTotal();
+    resetEntry();
+    dataTable(dataBase);
+
+    setTimeout(updateAlert, 200);
+  } else {
+    dataBase.push({ catagory, discription, amount, entryDate });
+    addData();
+    updateTotal();
+    resetEntry();
+    dataTable(dataBase);
+
+    setTimeout(addAlert, 200);
+  }
 }
 
-function deleteData(index) {
-  dataBase.splice(index, 1);
-  dataTable(dataBase);
-  addData();
-  updateTotal();
+// Function to Create Table
+function dataTable(filteredData) {
+  transactionData.innerHTML = "";
+
+  filteredData.forEach((data, index) => {
+    let table = document.getElementById("transactionData");
+    let newRow = table.insertRow(table.length);
+
+    let cell1 = newRow.insertCell();
+    cell1.innerHTML = index + 1;
+
+    let cell2 = newRow.insertCell();
+    cell2.innerHTML = data.catagory === "income" ? data.amount : "--";
+
+    let cell3 = newRow.insertCell();
+    cell3.innerHTML = data.catagory === "expense" ? data.amount : "--";
+
+    let cell4 = newRow.insertCell();
+    cell4.innerHTML = data.discription;
+
+    let cell5 = newRow.insertCell();
+    cell5.innerHTML = data.entryDate;
+
+    let cell6 = newRow.insertCell();
+
+    // Creating Delete Button Inside the Table
+    let deleteBtn = document.createElement("button");
+    let editBtn = document.createElement("button");
+
+    deleteBtn.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
+    deleteBtn.setAttribute("id", "deleteBtn");
+
+    // Function to Delete the Transaction
+    deleteBtn.addEventListener("click", () => {
+      if (confirm("Are You Sure want to Delete the Transaction?")) {
+        dataBase.splice(index, 1);
+
+        addData();
+        updateTotal();
+        dataTable(dataBase);
+
+        setTimeout(deleteAlert, 200);
+      } else {
+        return;
+      }
+    });
+
+    cell6.appendChild(deleteBtn);
+
+    // Creating Edit Button Inside the Table
+    editBtn.innerHTML = `<i class="bi bi-pencil-square">`;
+    editBtn.setAttribute("id", "editBtn");
+    editBtn.style.marginLeft = "10px";
+
+    // Function to Edit the Transaction
+    editBtn.addEventListener("click", () => {
+      if (confirm("Are You Sure want to Edit/Update the Transaction?")) {
+        catagoryInput.value = data.catagory;
+        dateInput.value = data.entryDate;
+        amountInput.value = data.amount;
+        discriptionInput.value = data.discription;
+        isEditing = true;
+        editIndex = index;
+        addButton.textContent = "Update";
+      } else {
+        return;
+      }
+    });
+
+    cell6.appendChild(editBtn);
+  });
 }
 
-addButton.addEventListener("click", addTransaction);
+// Function to Show Transaction Add Alert Message
+function addAlert() {
+  alert("The Transaction has Been Added Successfully !");
+}
+
+// Function to Show Transaction Update Alert Message
+function updateAlert() {
+  alert("The Transaction has Been Updated Successfully !");
+}
+
+// Function to Show Transaction Delete Alert Message
+function deleteAlert() {
+  alert("The Transaction has Been Deleted Successfully !");
+}
+
+// Getting Total Income, Expense & Net Balance Values
+function updateTotal() {
+  totalIncome = dataBase
+    .filter((data) => data.catagory === "income")
+    .reduce((sum, data) => sum + data.amount, 0);
+  totalIncomeDisplay.textContent = `₹ ${totalIncome.toFixed(2)}`;
+
+  totalExpense = dataBase
+    .filter((data) => data.catagory === "expense")
+    .reduce((sum, data) => sum + data.amount, 0);
+  totalExpenseDisplay.textContent = `₹ ${totalExpense.toFixed(2)}`;
+
+  netBalance = totalIncome - totalExpense;
+  headerBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
+  formDashboardBalance.textContent = `₹ ${netBalance.toFixed(2)}`;
+}
+
+// Function to Reset the Entry Form after Clicking the Reset Button
+resetButton.addEventListener("click", resetEntry);
 
 function resetEntry() {
   catagoryInput.value = "";
@@ -130,8 +201,7 @@ function resetEntry() {
   dateInput.value = "";
 }
 
-resetButton.addEventListener("click", resetEntry);
-
+// Function for Applying Filters after Clicking the Apply Button above the Data Table
 applyFilterBtn.addEventListener("click", () => {
   const selectedData = document.querySelector(
     'input[name="filter"]:checked'
@@ -139,8 +209,8 @@ applyFilterBtn.addEventListener("click", () => {
   const filteredData = dataBase.filter(
     (data) => selectedData === "all" || data.catagory === selectedData
   );
-
   dataTable(filteredData);
 });
 
+// Loading Data
 document.addEventListener("DOMContentLoaded", getData);
